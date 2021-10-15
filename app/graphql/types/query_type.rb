748b04825b472f875e83be94fa88ccbe21f6cbbd
Brett Jones <bhjones4545@gmail.com>
2021-10-15 14:57:35 -0600

    Add field method that queries all users
    Co-Authored-By: Caroline Tan <carolinectan3@gmail.com>

--- app/graphql/types/query_type.rb
+++ app/graphql/types/query_type.rb
@@ -3,11 +3,15 @@ module Types
     # Add root-level fields here.
     # They will be entry points for queries on your schema.
 
-    # TODO: remove me
-    field :test_field, String, null: false,
-      description: "An example field added by the generator"
-    def test_field
-      "Hello World!"
+    # field :user do
+    #   type UserType
+    #   argument :id, !types.String
+    #   resolve -> (root, args, ctx) {User.find(args[:id])}
+    # end
+    field :all_users, [UserType], null: false
+
+    def all_users
+      User.all
     end
   end
 end
