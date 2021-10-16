96190a6b35636cec51ccf9232e7e38ca71c21582
Caroline Tan <carolinectan3@gmail.com>
2021-10-16 18:48:51 -0400

    Add model validation tests
    Co-Authored-By: Brett Jones <bhjones4545@gmail.com>

--- spec/models/seed_spec.rb
+++ spec/models/seed_spec.rb
@@ -4,4 +4,23 @@ RSpec.describe Seed, type: :model do
   describe 'associations' do
     it { should belong_to(:user) }
   end
+
+  describe 'validations' do
+    it { should validate_presence_of(:name) }
+    it { should validate_length_of(:name).is_at_least(1).is_at_most(50) }
+    it { should validate_presence_of(:planting_depth) }
+    it { should validate_length_of(:planting_depth).is_at_least(1).is_at_most(20) }
+    it { should validate_presence_of(:days_to_germinate) }
+    it { should validate_length_of(:days_to_germinate).is_at_least(1).is_at_most(20) }
+    it { should validate_presence_of(:time_to_harvest) }
+    it { should validate_length_of(:time_to_harvest).is_at_least(1).is_at_most(20) }
+    it { should validate_presence_of(:date_planted) }
+    it { should validate_length_of(:date_planted).is_at_least(1).is_at_most(20) }
+    it { should validate_presence_of(:sun_exposure) }
+    it { should define_enum_for(:sun_exposure).with_values(['Full sun', 'Part shade', 'Full shade', 'Unknown']) }
+    it { should validate_presence_of(:expected_plant_height) }
+    it { should validate_length_of(:expected_plant_height).is_at_least(1).is_at_most(20) }
+    it { should validate_presence_of(:notes) }
+    it { should validate_length_of(:notes).is_at_least(1).is_at_most(140) }
+  end
 end
--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -5,4 +5,20 @@ RSpec.describe User, type: :model do
     it { should have_many(:seeds) }
     it { should have_many(:journal_entries) }
   end
+
+  describe 'validations' do
+    it { should validate_presence_of(:first_name) }
+    it { should validate_length_of(:first_name).is_at_least(1).is_at_most(50) }
+    it { should validate_presence_of(:last_name) }
+    it { should validate_length_of(:last_name).is_at_least(1).is_at_most(50) }
+
+    it { should validate_presence_of(:city) }
+    it { should validate_length_of(:city).is_at_least(1).is_at_most(50) }
+
+    it { should validate_presence_of(:state) }
+    it { should validate_length_of(:state).is_equal_to(2) }
+
+    it { should validate_presence_of(:intentions) }
+    it { should validate_length_of(:city).is_at_least(1).is_at_most(50) }
+  end
 end
