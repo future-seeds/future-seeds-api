78c62d5eb09259e13826126712b86d1a1e185dc1
Caroline Tan <carolinectan3@gmail.com>
2021-10-16 15:24:44 -0400

    Add seed and seeds methods; wip
    Co-Authored-By: Brett Jones <bhjones4545@gmail.com>

--- app/graphql/types/query_type.rb
+++ app/graphql/types/query_type.rb
@@ -17,24 +17,39 @@ module Types
     # def find_user(args)
     #   User.find(args)
     # end
-    #
-    # field :all_users, [UserType], null: false
-    #
-    # def all_users
-    #   User.all
-    # end
 
-    field :users, [UserType], null: false
+    field :users, [UserType], null: true do
+      description "get all users"
+    end
+
     field :user, UserType, null: true do
+      description "get user by id"
+      argument :id, ID, required: true
+    end
+
+    field :seeds, [SeedType], null: true do
+      description "get all seeds"
+    end
+
+    field :seed, SeedType, null: true do
+      description "get seed by id"
       argument :id, ID, required: true
     end
 
     def users
-      ::User.all
+      ::User.all # what is the :: syntax for?
     end
 
     def user(id:)
       ::User.find(id)
+    end
+
+    def seeds
+      ::Seed.all
+    end
+
+    def seed(id:)
+      ::Seed.find(id)
     end
   end
 end
--- app/graphql/types/user_type.rb
+++ app/graphql/types/user_type.rb
@@ -6,5 +6,16 @@ module Types
     field :city, String, null: false
     field :state, String, null: false
     field :intentions, String, null: false
+    field :seeds, [SeedType], null: true do
+      field :id, ID, null: false
+      field :name, String, null: false
+      field :planting_depth, String, null: false
+      field :days_to_germinate, String, null: false
+      field :time_to_harvest, String, null: false
+      field :date_planted, String, null: false
+      field :sun_exposure, Boolean, null: false
+      field :expected_plant_height, String, null: false
+      field :notes, String, null: false
+    end
   end
 end
