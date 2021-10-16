801d0ef3da289260e1781067c45cf82f7a7a015e
Caroline Tan <carolinectan3@gmail.com>
2021-10-16 15:21:31 -0400

    Add shoulamatchers; comment out config factory bot
    Co-Authored-By: Brett Jones <bhjones4545@gmail.com>

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -93,5 +93,5 @@ RSpec.configure do |config|
   # as the one that triggered the failure.
   Kernel.srand config.seed
 =end
-  config.include FactoryBot::Syntax::Methods
+  # config.include FactoryBot::Syntax::Methods
 end
