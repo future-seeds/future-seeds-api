a6abed5c3892152b5a908dd7b493802a22463fe4
Caroline Tan <carolinectan3@gmail.com>
2021-10-16 15:24:13 -0400

    Add id, user id, and user/type to seed
    Co-Authored-By: Brett Jones <bhjones4545@gmail.com>

--- app/graphql/types/seed_type.rb
+++ app/graphql/types/seed_type.rb
@@ -1,5 +1,6 @@
 module Types
   class SeedType < Types::BaseObject
+    field :id, ID, null: false
     field :name, String, null: false
     field :planting_depth, String, null: false
     field :days_to_germinate, String, null: false
@@ -8,5 +9,7 @@ module Types
     field :sun_exposure, Boolean, null: false
     field :expected_plant_height, String, null: false
     field :notes, String, null: false
+    field :user_id, ID, null: false
+    field :user, UserType, null: false
   end
 end
