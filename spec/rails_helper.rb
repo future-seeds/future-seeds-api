5b0d055ccd10fbe7fad33791ed6cb5963b6dd2ec
Brett Jones <bhjones4545@gmail.com>
2021-10-17 10:38:07 -0600

    Simplcov filters
    Co-Authored-By: Caroline Tan <carolinectan3@gmail.com>

--- spec/rails_helper.rb
+++ spec/rails_helper.rb
@@ -6,7 +6,12 @@ require File.expand_path('../config/environment', __dir__)
 abort("The Rails environment is running in production mode!") if Rails.env.production?
 require 'rspec/rails'
 # Add additional requires below this line. Rails is not loaded until this point!
-
+require 'simplecov'
+SimpleCov.start do
+  add_filter 'spec/'
+  add_filter 'app/channels/'
+  add_filter 'app/jobs/'
+end
 # Requires supporting ruby files with custom matchers and macros, etc, in
 # spec/support/ and its subdirectories. Files matching `spec/**/*_spec.rb` are
 # run as spec files by default. This means that files in spec/support that end
