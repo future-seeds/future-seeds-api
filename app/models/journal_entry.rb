d9c551765cc08e4de9be5cb4341d8b5f35ad5716
Brett Jones <bhjones4545@gmail.com>
2021-10-17 12:56:38 -0600

    Increase description maximum character validation
    Co-Authored-By: Caroline Tan <carolinectan3@gmail.com>

--- app/models/journal_entry.rb
+++ app/models/journal_entry.rb
@@ -2,5 +2,5 @@ class JournalEntry < ApplicationRecord
   belongs_to :user
 
   validates :date, presence: true, length: { is: 10}
-  validates :description, presence: true, length: { in: 1..50}
+  validates :description, presence: true, length: { in: 1..500}
 end
--- spec/models/journal_entry_spec.rb
+++ spec/models/journal_entry_spec.rb
@@ -10,6 +10,6 @@ RSpec.describe JournalEntry, type: :model do
     it { should validate_length_of(:date).is_equal_to(10) }
 
     it { should validate_presence_of(:description) }
-    it { should validate_length_of(:description).is_at_least(1).is_at_most(50) }
+    it { should validate_length_of(:description).is_at_least(1).is_at_most(500) }
   end
 end
