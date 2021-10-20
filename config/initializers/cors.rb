6594018d23d6849b60ecddf1655a818334d4a4a9
Caroline Tan <carolinectan3@gmail.com>
2021-10-20 17:22:59 -0400

    Change origin to localhost:3000

--- config/initializers/cors.rb
+++ config/initializers/cors.rb
@@ -7,7 +7,7 @@
 
 Rails.application.config.middleware.insert_before 0, Rack::Cors do
   allow do
-    origins 'https://future-seeds-api.herokuapp.com'
+    origins 'https://localhost:3000'
 
     resource '*',
       headers: :any,
