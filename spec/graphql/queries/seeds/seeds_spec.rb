428bd669d6631f903aed0fbfa94e490da85377bb
Brett Jones <bhjones4545@gmail.com>
2021-10-21 16:53:50 -0600

    Refactor expect for sun exposure
    Co-Authored-By: Caroline Tan <carolinectan3@gmail.com>

--- spec/graphql/queries/seeds/seeds_spec.rb
+++ spec/graphql/queries/seeds/seeds_spec.rb
@@ -4,7 +4,7 @@ RSpec.describe Types::QueryType do
   describe 'display seeds' do
     it 'can query all seeds' do
       user1 = create(:user, first_name: "Brett", last_name: "Tan", city: "San Diego", state: "CA", intentions: "To control the food supply")
-      seed1 = create(:seed, name: "Eggplant", planting_depth: "1-2 in", days_to_germinate: "5-7 days", time_to_harvest: "45-60 days", date_planted: "2021-11-23", expected_plant_height: "45-60 in", notes: "purple", sun_exposure: "full_sun", user: user1)
+      seed1 = create(:seed, name: "Eggplant", planting_depth: "1-2 in", days_to_germinate: "5-7 days", time_to_harvest: "45-60 days", date_planted: "2021-11-23", expected_plant_height: "45-60 in", notes: "purple", sun_exposure: "Full sun", user: user1)
       create_list(:seed, 4)
 
       result = FutureSeedsApiSchema.execute(all_seeds_query).as_json
@@ -17,7 +17,7 @@ RSpec.describe Types::QueryType do
       expect(result["data"]["seeds"].first["expectedPlantHeight"]).to eq("45-60 in")
       expect(result["data"]["seeds"].first["notes"]).to eq("purple")
       expect(result["data"]["seeds"].first["userId"]).to eq("#{user1.id}")
-      expect(result["data"]["seeds"].first["sunExposure"]).to eq(1)
+      expect(result["data"]["seeds"].first["sunExposure"]).to eq("FULLSUN")
     end
 
     def all_seeds_query
