6b06c1bf99b153ecd8527304e0204d58b6f11acd
Brett Jones <bhjones4545@gmail.com>
2021-10-21 17:18:31 -0600

    Create query for a single journal entry test
    Co-Authored-By: Caroline Tan <carolinectan3@gmail.com>

--- spec/graphql/queries/journal_entries/journal_entries_spec.rb
+++ spec/graphql/queries/journal_entries/journal_entries_spec.rb
@@ -24,4 +24,28 @@ RSpec.describe Types::QueryType do
       GQL
     end
   end
+
+  describe 'display a journal entry' do
+    it 'can query one journal entry' do
+      user1 = create(:user, first_name: "Brett", last_name: "Tan", city: "San Diego", state: "CA", intentions: "To control the food supply")
+      je1 = create(:journal_entry, id: 1, date: "2021-11-23", description: "The frost eliminated everything", user: user1)
+      create_list(:journal_entry, 1)
+
+      result = FutureSeedsApiSchema.execute(a_journal_entry_query).as_json
+      expect(result["data"]["journalEntry"].count).to eq(2)
+      expect(result["data"]["journalEntry"]["date"]).to eq("2021-11-23")
+      expect(result["data"]["journalEntry"]["description"]).to eq("The frost eliminated everything")
+    end
+
+    def a_journal_entry_query
+      <<~GQL
+      {
+        journalEntry(id:1){
+          date
+          description
+        }
+      }
+      GQL
+    end
+  end
 end
