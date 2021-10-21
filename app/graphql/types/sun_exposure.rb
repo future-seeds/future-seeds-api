89e50cc41ce72a3a999a8a5dc7e4d2e1f4c12005
Brett Jones <bhjones4545@gmail.com>
2021-10-21 16:52:59 -0600

    Create sun expoosure type enum values
    Co-Authored-By: Caroline Tan <carolinectan3@gmail.com>

---
+++ app/graphql/types/sun_exposure.rb
@@ -0,0 +1,6 @@
+class Types::SunExposure < Types::BaseEnum
+  value 'FULLSUN', value: 'Full sun'
+  value 'PARTSHADE', value: 'Part shade'
+  value 'FULLSHADE', value: 'Full shade'
+  value 'UNKNOWN', value: 'Unknown'
+end
