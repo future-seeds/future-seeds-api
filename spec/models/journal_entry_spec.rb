062d2a8984a6a359a643c90c60e11629ad56d2dd
Brett Jones <bhjones4545@gmail.com>
2021-10-21 17:31:46 -0600

    Create factory bot creation test for journal entry
    Co-Authored-By: Caroline Tan <carolinectan3@gmail.com>

--- spec/models/journal_entry_spec.rb
+++ spec/models/journal_entry_spec.rb
@@ -12,4 +12,15 @@ RSpec.describe JournalEntry, type: :model do
     it { should validate_presence_of(:description) }
     it { should validate_length_of(:description).is_at_least(1).is_at_most(500) }
   end
+
+  describe 'factory bot creation' do
+    before(:all) do
+      @user1 = create(:user)
+      @journal_entry1 = create(:journal_entry, user: @user1)
+    end
+
+    it 'is valid with attributes' do
+      expect(@journal_entry1).to be_valid
+    end
+  end
 end
