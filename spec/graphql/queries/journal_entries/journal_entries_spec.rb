4c45f7f9caaae1da836733f3e25583fbbfba673e
Brett Jones <bhjones4545@gmail.com>
2021-10-23 13:00:17 -0600

    Law abiding rubocop citizen
    Co-Authored-By: Caroline Tan <carolinectan3@gmail.com>

--- spec/graphql/queries/journal_entries/journal_entries_spec.rb
+++ spec/graphql/queries/journal_entries/journal_entries_spec.rb
@@ -7,48 +7,51 @@ RSpec.describe Types::QueryType do
       Seed.destroy_all
       User.destroy_all
 
-      user1 = create(:user, first_name: "Brett", last_name: "Tan", city: "San Diego", state: "CA", intentions: "To control the food supply")
-      je1 = create(:journal_entry, date: "2021-11-23", description: "The frost eliminated everything", user: user1)
+      user1 = create(:user, first_name: 'Brett', last_name: 'Tan', city: 'San Diego', state: 'CA',
+                            intentions: 'To control the food supply')
+      je1 = create(:journal_entry, date: '2021-11-23', description: 'The frost eliminated everything', user: user1)
       create_list(:journal_entry, 4)
 
       result = FutureSeedsApiSchema.execute(all_entries_query).as_json
-      expect(result["data"]["journalEntries"].count).to eq(5)
-      expect(result["data"]["journalEntries"].first["date"]).to eq("2021-11-23")
-      expect(result["data"]["journalEntries"].first["description"]).to eq("The frost eliminated everything")
+      expect(result['data']['journalEntries'].count).to eq(5)
+      expect(result['data']['journalEntries'].first['date']).to eq('2021-11-23')
+      expect(result['data']['journalEntries'].first['description']).to eq('The frost eliminated everything')
     end
 
     def all_entries_query
       <<~GQL
-      {
-        journalEntries{
-          date
-          description
+        {
+          journalEntries{
+            date
+            description
+          }
         }
-      }
       GQL
     end
   end
 
   describe 'display a journal entry' do
     it 'can query one journal entry' do
-      user1 = create(:user, first_name: "Brett", last_name: "Tan", city: "San Diego", state: "CA", intentions: "To control the food supply")
-      je1 = create(:journal_entry, id: 1, date: "2021-11-23", description: "The frost eliminated everything", user: user1)
+      user1 = create(:user, first_name: 'Brett', last_name: 'Tan', city: 'San Diego', state: 'CA',
+                            intentions: 'To control the food supply')
+      je1 = create(:journal_entry, id: 1, date: '2021-11-23', description: 'The frost eliminated everything',
+                                   user: user1)
       create_list(:journal_entry, 1)
 
       result = FutureSeedsApiSchema.execute(a_journal_entry_query).as_json
-      expect(result["data"]["journalEntry"].count).to eq(2)
-      expect(result["data"]["journalEntry"]["date"]).to eq("2021-11-23")
-      expect(result["data"]["journalEntry"]["description"]).to eq("The frost eliminated everything")
+      expect(result['data']['journalEntry'].count).to eq(2)
+      expect(result['data']['journalEntry']['date']).to eq('2021-11-23')
+      expect(result['data']['journalEntry']['description']).to eq('The frost eliminated everything')
     end
 
     def a_journal_entry_query
       <<~GQL
-      {
-        journalEntry(id:1){
-          date
-          description
+        {
+          journalEntry(id:1){
+            date
+            description
+          }
         }
-      }
       GQL
     end
   end
--- spec/graphql/queries/users/users_spec.rb
+++ spec/graphql/queries/users/users_spec.rb
@@ -6,47 +6,48 @@ RSpec.describe Types::QueryType do
       JournalEntry.destroy_all
       Seed.destroy_all
       User.destroy_all
-      
-      user1 = create(:user, first_name: "Brett", last_name: "Tan", city: "San Diego", state: "CA", intentions: "To control the food supply")
+
+      user1 = create(:user, first_name: 'Brett', last_name: 'Tan', city: 'San Diego', state: 'CA',
+                            intentions: 'To control the food supply')
       create_list(:user, 4)
 
       result = FutureSeedsApiSchema.execute(all_users_query).as_json
-      expect(result["data"]["users"].count).to eq(5)
-      expect(result["data"]["users"].first["firstName"]).to eq("Brett")
-      expect(result["data"]["users"].first["lastName"]).to eq("Tan")
-      expect(result["data"]["users"].first["city"]).to eq("San Diego")
-      expect(result["data"]["users"].first["state"]).to eq("CA")
-      expect(result["data"]["users"].first["intentions"]).to eq("To control the food supply")
+      expect(result['data']['users'].count).to eq(5)
+      expect(result['data']['users'].first['firstName']).to eq('Brett')
+      expect(result['data']['users'].first['lastName']).to eq('Tan')
+      expect(result['data']['users'].first['city']).to eq('San Diego')
+      expect(result['data']['users'].first['state']).to eq('CA')
+      expect(result['data']['users'].first['intentions']).to eq('To control the food supply')
     end
 
     def all_users_query
       <<~GQL
-      {
-        users{
-          firstName
-          lastName
-          city
-          state
-          intentions
+        {
+          users{
+            firstName
+            lastName
+            city
+            state
+            intentions
+          }
         }
-      }
       GQL
     end
   end
 
-
   describe 'display a user' do
     it 'can query one user' do
-      user1 = create(:user, id: 1, first_name: "Brett", last_name: "Tan", city: "San Diego", state: "CA", intentions: "To control the food supply")
+      user1 = create(:user, id: 1, first_name: 'Brett', last_name: 'Tan', city: 'San Diego', state: 'CA',
+                            intentions: 'To control the food supply')
       create_list(:user, 1)
 
       result = FutureSeedsApiSchema.execute(get_user_query).as_json
-      expect(result["data"]["user"].count).to eq(5)
-      expect(result["data"]["user"]["firstName"]).to eq("Brett")
-      expect(result["data"]["user"]["lastName"]).to eq("Tan")
-      expect(result["data"]["user"]["city"]).to eq("San Diego")
-      expect(result["data"]["user"]["state"]).to eq("CA")
-      expect(result["data"]["user"]["intentions"]).to eq("To control the food supply")
+      expect(result['data']['user'].count).to eq(5)
+      expect(result['data']['user']['firstName']).to eq('Brett')
+      expect(result['data']['user']['lastName']).to eq('Tan')
+      expect(result['data']['user']['city']).to eq('San Diego')
+      expect(result['data']['user']['state']).to eq('CA')
+      expect(result['data']['user']['intentions']).to eq('To control the food supply')
     end
 
     def get_user_query
