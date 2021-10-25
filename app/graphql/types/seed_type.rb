e75def25be4731a80de3f20b4e7270d3d1fc7e19
Caroline Tan <carolinectan3@gmail.com>
2021-10-25 19:40:40 -0400

    Change null to true
    Co-Authored-By: Brett Jones <bhjones4545@gmail.com>

--- app/graphql/types/seed_type.rb
+++ app/graphql/types/seed_type.rb
@@ -1,15 +1,15 @@
 module Types
   class SeedType < Types::BaseObject
     field :id, ID, null: false
-    field :name, String, null: false
-    field :planting_depth, String, null: false
-    field :days_to_germinate, String, null: false
-    field :time_to_harvest, String, null: false
-    field :date_planted, String, null: false
-    field :sun_exposure, Types::SunExposure, null: false
-    field :expected_plant_height, String, null: false
-    field :notes, String, null: false
-    field :user_id, ID, null: false
+    field :name, String, null: true
+    field :planting_depth, String, null: true
+    field :days_to_germinate, String, null: true
+    field :time_to_harvest, String, null: true
+    field :date_planted, String, null: true
+    field :sun_exposure, Types::SunExposure, null: true
+    field :expected_plant_height, String, null: true
+    field :notes, String, null: true
+    field :user_id, ID, null: true
     # field :user, UserType, null: false
   end
 end
