22e6a3d908af8d3f21321d99facc1afdbb6484a8
Caroline Tan <carolinectan3@gmail.com>
2021-10-25 19:35:55 -0400

    Refactor module inheritance
    Co-Authored-By: Brett Jones <bhjones4545@gmail.com>

--- spec/requests/create_user_spec.rb
+++ spec/requests/create_user_spec.rb
@@ -1,40 +1,41 @@
 require 'rails_helper'
-module Mutations
-  RSpec.describe CreateUser, type: :request do
-    it 'creates a user' do
-      Seed.destroy_all
-      JournalEntry.destroy_all
-      User.destroy_all
-      post '/graphql', params: {query: mutation}
 
-      expect(response).to be_successful
-      user = User.last
-      expect(user.first_name).to eq('Brett')
-      expect(user.last_name).to eq('Toensing')
-      expect(user.city).to eq('Atlanta')
-      expect(user.state).to eq('GA')
-      expect(user.intentions).to eq('Not cut trees')
-    end
+RSpec.describe Mutations::CreateUser, type: :request do
+  it 'creates a user' do
+    Seed.destroy_all
+    JournalEntry.destroy_all
+    User.destroy_all
 
-    def mutation
-      <<~GQL
-        mutation{
-          createUser(input: {
-              firstName: "Brett"
-              lastName: "Toensing"
-              city: "Atlanta"
-              state: "GA"
-              intentions: "Not cut trees"
-            })
-                {
-                firstName
-                lastName
-                city
-                state
-                intentions
-              }
-        }
-      GQL
-    end
+    post '/graphql', params: {query: mutation}
+
+    expect(response).to be_successful
+
+    user = User.last
+    expect(user.first_name).to eq('Brett')
+    expect(user.last_name).to eq('Toensing')
+    expect(user.city).to eq('Atlanta')
+    expect(user.state).to eq('GA')
+    expect(user.intentions).to eq('Not cut trees')
+  end
+
+  def mutation
+    <<~GQL
+      mutation{
+        createUser(input: {
+            firstName: "Brett"
+            lastName: "Toensing"
+            city: "Atlanta"
+            state: "GA"
+            intentions: "Not cut trees"
+          })
+              {
+              firstName
+              lastName
+              city
+              state
+              intentions
+            }
+      }
+    GQL
   end
 end
