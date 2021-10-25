0779411a478d751e0f9f40fa297cf3c1e59207f9
Brett Jones <bhjones4545@gmail.com>
2021-10-25 15:09:53 -0600

    refactor mutation type request
    Co-Authored-By: Caroline Tan <carolinectan3@gmail.com>

--- spec/requests/create_user_spec.rb
+++ spec/requests/create_user_spec.rb
@@ -1,40 +1,41 @@
 require 'rails_helper'
+module Mutations
+  RSpec.describe CreateUser, type: :request do
+    it 'creates a user' do
+      Seed.destroy_all
+      JournalEntry.destroy_all
+      User.destroy_all
+      post '/graphql', params: {query: mutation}
 
-RSpec.describe 'create user' do
-  it 'creates a user' do
-    post '/graphql', params: {query: mutation}
+      expect(response).to be_successful
+      user = User.last
 
-    expect(response).to be_successful
+      expect(user.first_name).to eq('Brett')
+      expect(user.last_name).to eq('Toensing')
+      expect(user.city).to eq('Atlanta')
+      expect(user.state).to eq('GA')
+      expect(user.intentions).to eq('Not cut trees')
+    end
 
-    user = User.last
-
-    expect(user.first_name).to eq('Brett')
-    expect(user.last_name).to eq('Toensing')
-    expect(user.city).to eq('Atlanta')
-    expect(user.state).to eq('GA')
-    expect(user.intentions).to eq('Not cut trees')
-  end
-
-  def mutation
-    <<~GQL
-      mutation{
-        createUser(input:
-          {
-            firstName: "Brett"
-            lastName: "Toensing"
-            city: "Atlanta"
-            state: "GA"
-            intentions: "Not cut trees"
-          }) {
-            user {
-              firstName
-              lastName
-              city
-              state
-              intentions
-            }
-          }
-      }
-    GQL
+    def mutation
+      <<~GQL
+        mutation{
+          createUser(input: {
+              firstName: "Brett"
+              lastName: "Toensing"
+              city: "Atlanta"
+              state: "GA"
+              intentions: "Not cut trees"
+            })
+                {
+                firstName
+                lastName
+                city
+                state
+                intentions
+              }
+        }
+      GQL
+    end
   end
 end
