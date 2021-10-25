3d376ab39d081eca0a79a95923848a97ebf68451
Brett Jones <bhjones4545@gmail.com>
2021-10-25 15:10:20 -0600

    Change null to true
    Co-Authored-By: Caroline Tan <carolinectan3@gmail.com>

--- app/graphql/types/user_type.rb
+++ app/graphql/types/user_type.rb
@@ -1,15 +1,15 @@
 module Types
   class UserType < Types::BaseObject
     field :id, ID, null: false
-    field :first_name, String, null: false
-    field :last_name, String, null: false
-    field :city, String, null: false
-    field :state, String, null: false
-    field :intentions, String, null: false
+    field :first_name, String, null: true
+    field :last_name, String, null: true
+    field :city, String, null: true
+    field :state, String, null: true
+    field :intentions, String, null: true
 
-    field :seeds, [SeedType], null: false
+    field :seeds, [SeedType], null: true
 
-    field :journal_entries, [JournalEntryType], null: false
-    field :journal_entry, JournalEntryType, null: false
+    field :journal_entries, [JournalEntryType], null: true
+    field :journal_entry, JournalEntryType, null: true
   end
 end
