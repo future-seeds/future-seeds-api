2ccf9780f09be7aa32c070248ea930cb7fde424f
Caroline Tan <carolinectan3@gmail.com>
2021-10-25 21:35:44 -0400

    Add create journal entry mutation
    Co-Authored-By: Brett Jones <bhjones4545@gmail.com>

---
+++ app/graphql/mutations/create_journal_entry.rb
@@ -0,0 +1,28 @@
+class Mutations::CreateJournalEntry < ::Mutations::BaseMutation
+  argument :id, ID, required: false
+  argument :date, String, required: false
+  argument :description, String, required: false
+  argument :user_id, ID, required: false
+
+  type Types::JournalEntryType
+
+  field :journal_entry, Types::JournalEntryType, null: false
+  field :errors, [String], null: false
+
+  def resolve(date:, description:, user_id:)
+    journal_entry = JournalEntry.create!(date: date,
+                                         description: description,
+                                         user_id: user_id)
+
+    if journal_entry.save
+      {
+        journal_entry: journal_entry
+      }
+    else
+      {
+        journal_entry: nil,
+        errors: journal_entry.errors.full_messages
+      }
+    end
+  end
+end
