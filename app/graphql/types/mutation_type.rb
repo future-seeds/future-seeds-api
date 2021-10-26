95a39fe01ef1de8378ce1100dba0e96707a314a1
Caroline Tan <carolinectan3@gmail.com>
2021-10-25 21:36:58 -0400

    Add create journal entry field in mutation type
    Co-Authored-By: Brett Jones <bhjones4545@gmail.com>

--- app/graphql/types/mutation_type.rb
+++ app/graphql/types/mutation_type.rb
@@ -2,5 +2,6 @@ module Types
   class MutationType < Types::BaseObject
     field :create_user, mutation: Mutations::CreateUser
     field :create_seed, mutation: Mutations::CreateSeed
+    field :create_journal_entry, mutation: Mutations::CreateJournalEntry
   end
 end
