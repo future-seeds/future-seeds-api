fe42e6c7fcfc42e8092c1952eb0b6a4ca573907c
Caroline Tan <carolinectan3@gmail.com>
2021-10-26 01:26:36 -0400

    Add frozen string literal comment to satisfy RuboCop

--- app/models/journal_entry.rb
+++ app/models/journal_entry.rb
@@ -1,3 +1,5 @@
+# frozen_string_literal: true
+
 class JournalEntry < ApplicationRecord
   belongs_to :user
 
--- app/models/seed.rb
+++ app/models/seed.rb
@@ -1,3 +1,5 @@
+# frozen_string_literal: true
+
 class Seed < ApplicationRecord
   belongs_to :user
 
