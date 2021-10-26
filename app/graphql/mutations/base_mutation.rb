228eef925445a52b45aa419999ae0047249364eb
Caroline Tan <carolinectan3@gmail.com>
2021-10-26 15:27:19 -0400

    Change ::RelayClassicMutation to ::Mutation
    Co-Authored-By: Brett Jones <bhjones4545@gmail.com>

--- app/graphql/mutations/base_mutation.rb
+++ app/graphql/mutations/base_mutation.rb
@@ -1,5 +1,5 @@
 module Mutations
-  class BaseMutation < GraphQL::Schema::RelayClassicMutation
+  class BaseMutation < GraphQL::Schema::Mutation
     # argument_class Types::BaseArgument
     # field_class Types::BaseField
     # input_object_class Types::BaseInputObject
