82766cfde5759fff666effe37c5a7cf661794848
Brett Jones <bhjones4545@gmail.com>
2021-10-26 17:35:55 -0600

    Remove unnecessary enum file
    Co-Authored-By: Caroline Tan <carolinectan3@gmail.com>

--- app/graphql/types/sun_exposure.rb
+++
@@ -1,8 +0,0 @@
-# frozen_string_literal: true
-
-class Types::SunExposure < Types::BaseEnum
-  value 'FULLSUN', value: 'Full sun'
-  value 'PARTSHADE', value: 'Part shade'
-  value 'FULLSHADE', value: 'Full shade'
-  value 'UNKNOWN', value: 'Unknown'
-end
